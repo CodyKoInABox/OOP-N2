ccafbb50b8958e43bebfca07f23c02346be1c5a9
Cody Ko In A Box <125526050+CodyKoInABox@users.noreply.github.com>
2023-04-29 00:05:54 -0300

    Add files via upload

---
+++ Disciplina.java
@@ -0,0 +1,84 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Disciplina {
+    
+    private String codigo;
+
+    private String nome;
+
+    private int cargaHoraria;
+
+    private String ementa;
+
+    private Professor professor;
+
+    private List<Matriculado> listaDeMatriculados;
+
+    public void adicionarMatriculado(Matriculado matriculado){
+        listaDeMatriculados.add(matriculado);
+    }
+
+    public Disciplina() {
+    }
+
+    public Disciplina(String codigo, String nome, int cargaHoraria, String ementa, Professor professor) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+        this.ementa = ementa;
+        this.professor = professor;
+        this.listaDeMatriculados = new ArrayList<Matriculado>();
+    }
+
+    public String getCodigo() {
+        return this.codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCargaHoraria() {
+        return this.cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getEmenta() {
+        return this.ementa;
+    }
+
+    public void setEmenta(String ementa) {
+        this.ementa = ementa;
+    }
+
+    public Professor getProfessor() {
+        return this.professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public List<Matriculado> getListaDeMatriculados() {
+        return this.listaDeMatriculados;
+    }
+
+    public void setListaDeMatriculados(List<Matriculado> listaDeMatriculados) {
+        this.listaDeMatriculados = listaDeMatriculados;
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+
+        Aluno aluno = new Aluno("1000", "Jose", "Jopinville", "0800", "gmail");
+
+        System.out.println(aluno.getNome());
+    }
+}
